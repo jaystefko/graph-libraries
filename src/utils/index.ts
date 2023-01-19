fe0df70612f85c15f682ee3b9ec9c4adf87380ae
JakubStefko <jakub.stefko@gmail.com>
2023-01-19 09:57:09 +0100

    better label formatting

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,8 +8,8 @@ export function formatLabel(option: Option, timestamp: number) {
   const date = new Date(timestamp);
 
   if (option === 1) {
-    return `${unifyLabel(date.getHours())}.${unifyLabel(date.getMinutes())}`;
+    return `${unifyLabel(date.getHours())}:${unifyLabel(date.getMinutes())}`;
   }
 
-  return `${unifyLabel(date.getDate())}.${unifyLabel(date.getMonth() + 1)}`;
+  return `${unifyLabel(date.getDate())}/${unifyLabel(date.getMonth() + 1)}`;
 }
